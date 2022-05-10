8954bf7e75d95aed57b6e7dbaf402231aac7337b
Vitorsdl <59091198+Vitorsdl@users.noreply.github.com>
2022-05-10 15:41:23 -0300

    Add files via upload

---
+++ terceiro_desafio.java
@@ -0,0 +1,28 @@
+package padrao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class terceiro_desafio {
+	public static void main(String[] args) throws Exception {
+
+		  Scanner valor = new Scanner(System.in);        
+
+		  int alvo;
+		  int [] array;
+		  
+		  System.out.printf("Valor alvo:");
+		  alvo = valor.nextInt();
+		            
+		  Scanner valores = new Scanner(System.in);
+		   
+		  System.out.println("Numeros da lista:");
+		  array = new int[valores.nextInt()];
+		  
+		  for (int i = 0; i < ; i++) {
+		  }
+
+
+
+}
